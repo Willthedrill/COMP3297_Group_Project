5cdf4ccfd9ad9a0b8f2061ed9505ea95410523ca
llh66 <61980686+llh66@users.noreply.github.com>
2022-04-17 16:19:55 +0800

    Update members.py

--- orders/members.py
+++ orders/members.py
@@ -1,28 +1,35 @@
 from .models import MemberRecord
-
+import json
 def InsertMember(uid, name):
     try:
         NewMember = MemberRecord.get(uid = uid)
     except:
         if len(uid) != 10 or not uid.isnumeric():
-            return "Error: invalid HKU ID format, should be 10 numbers."
+            message = json.dump({"hkuid":uid, "message":"Error: invalid HKU ID format, should be 10 numbers."})
+            return message
         if len(name) == 0 or len(name) > 150:
-            return "Error: invalid name length, should be 1 - 150."
+            message = json.dump({"hkuid":uid, "message":"Error: invalid name length, should be 1 - 150."})
+            return message
         NewMember = MemberRecord(uid = uid, name = name)
         NewMember.save()
-        return "HKU member record created successfully."
-    return "Error: record with such HKU ID already exists"
+        message = json.dump({"hkuid":uid, "message":"HKU member record created successfully."})
+        return message
+    message = json.dump({"hkuid":uid, "message":"Error: record with such HKU ID already exists."})
+    return message
 
 def UpdateMember(uid, name):
     try:
         member = MemberRecord.get(uid = uid)
     except:
-        return "Error: given HKU ID does not match any records"
+        message = json.dump({"hkuid":uid, "message":"Error: given HKU ID does not match any records."})
+        return message
     if len(name) == 0 or len(name) > 150:
-        return "Error: invalid name length, should be 1 - 150."
+        message = json.dump({"hkuid":uid, "message":"Error: invalid name length, should be 1 - 150."})
+        return message
     member.name = name
     member.save()
-    return "HKU member record updated successfully."
+    message = json.dump({"hkuid":uid, "message":"HKU member record updated successfully."})
+    return message
 
 def SearchMember(key):
     try:
@@ -31,20 +38,28 @@ def SearchMember(key):
         try:
             member = MemberRecord.get(name = key)
         except:
-            return "Error: given HKU ID or name does not match any records"
-    return member
+            message = json.dump({"hkuid":uid, "message":"Error: given HKU ID or name does not match any records."})
+            return message
+        message = json.dump(member)
+        return message
+    message = json.dump(member)
+    return message
 
 def DeleteMember(uid):
     try:
         member = MemberRecord.get(uid = uid)
     except:
-        return "Error: given HKU ID does not match any records"
+        message = json.dump({"hkuid":uid, "message":"Error: given HKU ID does not match any records."})
+        return message
     member.delete()
-    return "HKU member record deleted successfully."
+    message = json.dump({"hkuid":uid, "message":"HKU member record deleted successfully."})
+    return message
 
 def ListAllMember():
     members = MemberRecord.objects.all()
     if len(members) == 0:
-        return "Warning: no record available"
+        message = json.dump({"hkuid":uid, "Warning: no record available."})
+        return message
     else:
-        return members
+        message = json.dump(members)
+        return message
