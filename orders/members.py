371d265e6424822618563cff3eae25b5ac451433
llh66 <61980686+llh66@users.noreply.github.com>
2022-04-17 21:48:25 +0800

    Update members.py

--- orders/members.py
+++ orders/members.py
@@ -5,30 +5,30 @@ def InsertMember(uid, name):
         NewMember = MemberRecord.objects.get(hkuid = uid)
     except:
         if len(uid) != 10 or not uid.isnumeric():
-            message = json.dump({"hkuid":uid, "message":"Error: invalid HKU ID format, should be 10 numbers."})
+            message = json.dumps({"hkuid":uid, "message":"Error: invalid HKU ID format, should be 10 numbers."})
             return message
         if len(name) == 0 or len(name) > 150:
-            message = json.dump({"hkuid":uid, "message":"Error: invalid name length, should be 1 - 150."})
+            message = json.dumps({"hkuid":uid, "message":"Error: invalid name length, should be 1 - 150."})
             return message
         NewMember = MemberRecord(uid = uid, name = name)
         NewMember.save()
-        message = json.dump({"hkuid":uid, "message":"HKU member record created successfully."})
+        message = json.dumps({"hkuid":uid, "message":"HKU member record created successfully."})
         return message
-    message = json.dump({"hkuid":uid, "message":"Error: record with such HKU ID already exists."})
+    message = json.dumps({"hkuid":uid, "message":"Error: record with such HKU ID already exists."})
     return message
 
 def UpdateMember(uid, name):
     try:
         member = MemberRecord.objects.get(hkuid = uid)
     except:
-        message = json.dump({"hkuid":uid, "message":"Error: given HKU ID does not match any records."})
+        message = json.dumps({"hkuid":uid, "message":"Error: given HKU ID does not match any records."})
         return message
     if len(name) == 0 or len(name) > 150:
-        message = json.dump({"hkuid":uid, "message":"Error: invalid name length, should be 1 - 150."})
+        message = json.dumps({"hkuid":uid, "message":"Error: invalid name length, should be 1 - 150."})
         return message
     member.name = name
     member.save()
-    message = json.dump({"hkuid":uid, "message":"HKU member record updated successfully."})
+    message = json.dumps({"hkuid":uid, "message":"HKU member record updated successfully."})
     return message
 
 def SearchMember(key):
@@ -38,28 +38,28 @@ def SearchMember(key):
         try:
             member = MemberRecord.get(name = key)
         except:
-            message = json.dump({"hkuid":uid, "message":"Error: given HKU ID or name does not match any records."})
+            message = json.dumps({"key":key, "message":"Error: given HKU ID or name does not match any records."})
             return message
-        message = json.dump(member)
+        message = json.dumps(member)
         return message
-    message = json.dump(member)
+    message = json.dumps(member)
     return message
 
 def DeleteMember(uid):
     try:
         member = MemberRecord.objects.get(hkuid = uid)
     except:
-        message = json.dump({"hkuid":uid, "message":"Error: given HKU ID does not match any records."})
+        message = json.dumps({"hkuid":uid, "message":"Error: given HKU ID does not match any records."})
         return message
     member.delete()
-    message = json.dump({"hkuid":uid, "message":"HKU member record deleted successfully."})
+    message = json.dumps({"hkuid":uid, "message":"HKU member record deleted successfully."})
     return message
 
 def ListAllMember():
     members = MemberRecord.objects.all()
     if len(members) == 0:
-        message = json.dump({"hkuid":uid, "Warning: no record available."})
+        message = json.dumps({"message":"Warning: no record available."})
         return message
     else:
-        message = json.dump(members)
+        message = json.dumps(members)
         return message
