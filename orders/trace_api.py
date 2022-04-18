46ff89fd71d2f22d03314a14886ef1211397b07e
adorama0219 <56353543+adorama0219@users.noreply.github.com>
2022-04-18 13:00:26 +0800

    Update trace_api.py

--- orders/trace_api.py
+++ orders/trace_api.py
@@ -11,6 +11,7 @@ def get_close_contact(request):
     str1=request.data
     str1=json.dumps(str1)
     query=json.loads(str1)
+#    list=query['content']
     uid= query['hkuid']
     d= query['datetime']
     date= datetime.strptime(d, '%Y-%m-%d %H:%M:%S')
@@ -19,15 +20,18 @@ def get_close_contact(request):
     result_venue=[]
     result=[]
     subject=[]
-    history=Record.objects.filter(hkuid=uid, type='entry')
+    history=Record.objects.filter(hkuid=uid, type='en')
+   # history=json.loads(filter)
+    print(history)
     for list in history:
-        datetime1= list['datetime']
+        print(list)
+        datetime1= list.datetime
         t=datetime.strptime(datetime1, '%Y-%m-%d %H:%M:%S')
         if (t.getDate()== date.getDate() or t.getDate()== beforeone.getDate() or t.getDate()== beforetwo.getDate()):
             result_venue.append(list['venue'])
         #result_venue= venues the infected person went to in the last two days
         for venue in result_venue:
-            close_contact=Record.objects.filter(type='entry', venue=venue)
+            close_contact=Record.objects.filter(type='en', venue=venue)
             for individual in close_contact:
                 datetime2= individual['datetime']
                 t2=datetime.strptime(datetime2, '%Y-%m-%d %H:%M:%S')
