75071d57f7d7e2e59bc16a1e6449bc808fd2732f
William Ye <thrive@connect.hku.hk>
2022-05-05 19:54:13 +0800

    change to applying api in studysafe trace-venue

--- studysafe_trace/views.py
+++ studysafe_trace/views.py
@@ -6,17 +6,39 @@ from studysafe_core.models import *
 from studysafe_core.serializers import *
 from rest_framework.response import Response
 from django.views.generic import TemplateView
-
+import json
+import requests
 class trace_venue(TemplateView):
+    
     template_name = "venues.html"
     def get_context_data(self, **kwargs):
+        def display_detail(response,return_df=False):
+            raw=json.loads(response.content.decode('utf-8'))
+            for i in range(len(raw)):
+                try:
+                    if not return_df:
+                        display(pd.DataFrame(raw[i]['data']))
+                except:
+                    display(raw[i])
+            if return_df:
+                try:
+                    return pd.DataFrame(raw[0]['data'])
+                except:
+                    pass
         import pandas as pd
         import datetime
         hkuid,infectious_time_str=self.kwargs['hkuid'],self.kwargs['infectious_time']
         infectious_time=pd.to_datetime(infectious_time_str,format="%Y-%m-%d_%H:%M:%S")
         start_infectious_time=infectious_time+datetime.timedelta(days=-2)
-        records = Record.objects.all().values()
-        records=pd.DataFrame(records)
+        local=True
+        params={"type":"list"}
+        remote_core_path="https://shielded-tor-28383.herokuapp.com/studysafe_core/api_device"
+        local_core_path='http://localhost:8000/studysafe_core'
+        if local:
+            response=requests.post(local_core_path+'/api_device',data=json.dumps(params),headers = {'content-type':'application/json'})
+        else:
+            response=requests.post(remote_core_path+'/api_device',data=json.dumps(params),headers = {'content-type':'application/json'})
+        records=display_detail(response,return_df=True)
         records['datetime']=records['datetime'].apply(lambda x:pd.to_datetime(x,format="%Y-%m-%d %H:%M:%S"))
         temp_records=records.loc[records['hkuid_id']==hkuid]
         venue_record=temp_records.loc[temp_records['datetime'].apply(lambda x:True if ((x<infectious_time)and (x>start_infectious_time)) else False)].sort_values('venue_code_id')
