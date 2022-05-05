c53ec788ec0f65afe9b0065a084712bcb3c7ab9a
201220007 <85940970+201220007@users.noreply.github.com>
2022-05-05 17:26:35 +0800

    Update urls.py

--- studysafe_trace/urls.py
+++ studysafe_trace/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('trace_venue/<str:hkuid>/<str:infectious_time>',views.trace_venue.as_view()),    
+    path('trace_venue/<str:hkuid>/<str:infectious_time>',views.trace_venue.as_view()),
+    path('contacts/<str:hkuid>/<str:datetime>', views.contacts),
 ]
 
